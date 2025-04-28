dc8f1b6e02331067bd942bbf5ac90844e9bd0d5c
leofelix <leofelixnam@gmail.com>
2025-04-29 00:39:54 +0800

    testing lint

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -22,7 +22,7 @@ export class ProductComponent {
     }),
     { defaultValue: undefined },
   );
-ngOnInit(){}
+  ngOnInit(){}
   updateProduct() {
     this.productId.set(this.selectedProductId);
   }
