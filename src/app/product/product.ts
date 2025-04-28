c6a2a11d7e576515db69ebda19f080e98a965b33
leofelix <leofelixnam@gmail.com>
2025-04-28 23:55:42 +0800

    update product

---
+++ src/app/product/product.ts
@@ -0,0 +1,10 @@
+export interface Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  images: string[];
+  rating: number;
+  category: string;
+  thumbnail: string;
+}
